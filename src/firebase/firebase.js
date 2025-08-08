b081a55acb4298e76b7b05deeed3ad6c670ee583
Abu Sufian Robin <150505286+abusufianrobin@users.noreply.github.com>
2025-08-08 15:01:52 +0600

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // Import getFirestore
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAO71vENxSX9Uhe2wbdPLRj2VCneMXAfD8", // REPLACE WITH YOUR ACTUAL API KEY
-  authDomain: "reactfirebase-b600d.firebaseapp.com", // REPLACE WITH YOUR ACTUAL AUTH DOMAIN
-  projectId: "reactfirebase-b600d", // REPLACE WITH YOUR ACTUAL PROJECT ID
-  storageBucket: "reactfirebase-b600d.firebasestorage.app", // REPLACE WITH YOUR ACTUAL STORAGE BUCKET
-  messagingSenderId: "1082642949965", // REPLACE WITH YOUR ACTUAL MESSAGING SENDER ID
-  appId: "1:1082642949965:web:10cab636ac969ff8017904" // REPLACE WITH YOUR ACTUAL APP ID
+  apiKey: // REPLACE WITH YOUR ACTUAL API KEY
+  authDomain: // REPLACE WITH YOUR ACTUAL AUTH DOMAIN
+  projectId: // REPLACE WITH YOUR ACTUAL PROJECT ID
+  storageBucket:  // REPLACE WITH YOUR ACTUAL STORAGE BUCKET
+  messagingSenderId:  // REPLACE WITH YOUR ACTUAL MESSAGING SENDER ID
+  appId:  // REPLACE WITH YOUR ACTUAL APP ID
 };
 
 const app = initializeApp(firebaseConfig);
